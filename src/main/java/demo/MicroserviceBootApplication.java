60bfc8757a39f8ba69b2f06f3f72d3891aa0b01f
fikireyobed@gmail.com <fikirdebele@gmail.com>
2017-02-25 16:16:52 -0600

    test

--- src/main/java/demo/MicroserviceBootApplication.java
+++ src/main/java/demo/MicroserviceBootApplication.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
 
 import demo.domain.SmartEntity;
 import demo.domain.Users;
@@ -18,19 +21,24 @@ import demo.service.TeamService;
 /*
  * used when run as a JAR
  */
+@SuppressWarnings("deprecation")
 @SpringBootApplication(scanBasePackages = {"demo"})
-public class MicroserviceBootApplication extends SpringBootServletInitializer {
+public class MicroserviceBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceBootApplication.class, args);
 	}
 /*
  * used when run as a WAR
  */
-	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(MicroserviceBootApplication.class);
 	}
-	
+	//@Override
+//	public void onStartup(ServletContext arg0) throws ServletException {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
 	
 	
 }
